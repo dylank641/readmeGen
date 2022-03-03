529457700b9f1ecaec09d6817b78d8d64bb4a7d4
James Killelea <dylan.killelea@yahoo.com>
2022-03-03 13:47:18 -0600

    for intruc

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,145 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const genMark = require('./utils/generateMarkdown');
+const fs = require('fs');
+
+//project title
+//description
+//installation instructions
+//usage information
+//contribution guidelines
+//test instructions
+//licence(options)
+//github username
+
+//Questions
+//email addrress goes under questions
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project? (Required)',
+      validate: title => {
+        if (title) {
+          return true;
+        } else {
+          console.log('Please enter the title!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a description of your project. (Required)',
+      validate: description => {
+        if (description) {
+          return true;
+        } else {
+          console.log('Please provide a description!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'installInstruc',
+      message: 'Please provide the installation instructions (Required)',
+      validate: installInstruc => {
+        if (installInstruc) {
+          return true;
+        } else {
+          console.log('Please provide the installation instructions!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'usageInfo',
+      message: 'Please provide the usage information. (Required)',
+      validate: usageInfo => {
+        if (usageInfo) {
+          return true;
+        } else {
+          console.log('Please provide the usage information!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'conGuide',
+      message: 'Please provide the contribution guidelines. (Required)',
+      validate: conGuide => {
+        if (conGuide) {
+          return true;
+        } else {
+          console.log('Please provide the contribution guidelines!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'testInstruc',
+      message: 'Please provide the test instructions. (Required)',
+      validate: testInstruc => {
+        if (testInstruc) {
+          return true;
+        } else {
+          console.log('Please provide the test intructions!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please provide the test instructions. (Required)',
+      choices: ['Apache 2.0 License', 'The MIT License', 'IBM Public License Version 1.0', 'Eclipse Public License 1.0', 'Boost Software License 1.0', 'None'],
+    },
+  
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(file, data) {
+  //send data to generate markdonw
+  //use fs to write it to file
+  return new Promise((resolve, reject) => {
+    fs.writeFile('README.md', questions, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+.then(function(answers){
+  console.log(answers);
+})
+.then(pageR => {
+  return writeToFile(pageR)
+})
+.catch(err => {
+  console.log(err);
+});
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,105 @@
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  switch(license){
+    case 'Apache 2.0 License':
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+      break;
+    case 'The MIT License':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      break;
+    case 'IBM Public License Version 1.0':
+      return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+      break;
+    case 'Eclipse Public License 1.0':
+      return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+      break;
+    case 'Boost Software License 1.0':
+      return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+      break;
+    case 'None':
+      return ''
+      break;
+  }
+}
+//['Apache 2.0 License', 'The MIT License', 'IBM Public License Version 1.0', 'Eclipse Public License 1.0', 'Boost Software License 1.0', 'None']
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license){
+    case 'Apache 2.0 License':
+      return `https://opensource.org/licenses/Apache-2.0`
+      break;
+    case 'The MIT License':
+      return 'https://opensource.org/licenses/MIT'
+      break;
+    case 'IBM Public License Version 1.0':
+      return 'https://opensource.org/licenses/IPL-1.0'
+      break;
+    case 'Eclipse Public License 1.0':
+      return 'https://opensource.org/licenses/EPL-1.0'
+      break;
+    case 'Boost Software License 1.0':
+      return 'https://www.boost.org/LICENSE_1_0.txt'
+      break;
+    case 'None':
+      return ''
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+module.exports = function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+ ${data.description}
+  
+  ## Table of Contents
+  
+  (If your README is long, add a table of contents to make it easy for users to find what they need.
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license))
+  
+  ## Installation
+  
+  ${data.installIntruc}
+  
+  ## Usage
+  
+  ${data.usageInfo}
+
+
+
+  ## License
+
+  ${renderLicenseLink(license)}
+
+
+  ## Contributing
+
+  ${data.conGuide}
+  
+  ## Tests
+
+  ${data.testIntruc}
+
+  ## Questions
+
+  link goes here
 
 `;
 }
 
-module.exports = generateMarkdown;
